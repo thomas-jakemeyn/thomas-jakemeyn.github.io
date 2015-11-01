53099ee626adc382521fd052bddb97c4a40a92f8
thomas_jakemeyn <thomas.jakemeyn@gmail.com>
2015-11-01 20:41:06 +0100

    Working on move logic

--- app/project/project.utils.js
+++ app/project/project.utils.js
@@ -7,6 +7,16 @@ class ProjectUtils {
     moveTaskToSprint(project, taskId, sprintId, beforeTaskId) {
         var task = this.findTask(project, taskId);
         task.sprint = sprintId;
+        this.moveTask(project, taskId, beforeTaskId);
+    }
+
+    moveTask(project, taskId, beforeTaskId) {
+        var task = this.findTask(project, taskId);
+        var beforeTask = beforeTaskId ? this.findTask(project, beforeTaskId) : null;
+        var tasks = project.backlog;
+        var fromIndex = tasks.indexOf(task);
+        var toIndex = beforeTask ? tasks.indexOf(beforeTask) : tasks.length - 1;
+        tasks.splice(toIndex, 0, tasks.splice(fromIndex, 1)[0]);
     }
 
     findTask(project, taskId) {
