736660f8f9a15d00e7b1ea3c6bb35e7a21893900
thomas_jakemeyn <thomas.jakemeyn@gmail.com>
2015-11-01 09:20:02 +0100

    Working on drag&drop

--- app/project/view/backlog/project-backlog.controller.js
+++ app/project/view/backlog/project-backlog.controller.js
@@ -1,9 +1,33 @@
 'use strict';
 
+import angular from 'angular';
+
 class ProjectBacklogController {
 
-    constructor($scope) {
+    constructor($scope, dragulaService) {
+        this.$scope = $scope;
+        this.dragulaService = dragulaService;
+        this.setUpDragAndDrop();
+    }
+
+    setUpDragAndDrop() {
+        this.$scope.$on('droppable.drop', (event, task, targetSprint, sourceSprint, beforeTask) => {
+            this.onTaskDropped(task, targetSprint, sourceSprint, beforeTask);
+        });
+        this.dragulaService.options(this.$scope, 'droppable', {
+            invalid: function (element) {
+                return !element.classList.contains('draggable');
+            }
+        });
+    }
+
+    onTaskDropped(task, targetSprint, sourceSprint, beforeTask) {
+        var taskId = task.attr('id');
+        var sourceSprintId = sourceSprint.attr('id');
+        var targetSprintId = targetSprint.attr('id');
+        var beforeTaskId = beforeTask ? beforeTask.attr('id') : null;
+        console.log('Moved task ' + taskId + ' from sprint ' + sourceSprintId + ' to sprint ' + targetSprintId);
     }
 }
 
-export default ['$scope', ProjectBacklogController];
+export default ['$scope', 'dragulaService', ProjectBacklogController];
--- app/project/view/board/project-board.controller.js
+++ app/project/view/board/project-board.controller.js
@@ -14,30 +14,30 @@ class ProjectBoardController {
 
     setUpDragAndDrop() {
         this.$scope.$on('lane.over', (event, task, lane) => {
-            this.onLaneHovered(event, task, lane);
+            this.onLaneHovered(lane);
         });
         this.$scope.$on('lane.out', (event, task, lane) => {
-            this.onLaneExited(event, task, lane);
+            this.onLaneExited(lane);
         });
         this.$scope.$on('lane.drop', (event, task, targetLane, sourceLane, beforeTask) => {
-            this.onTaskDropped(event, task, targetLane, sourceLane, beforeTask);
+            this.onTaskDropped(task, targetLane, sourceLane, beforeTask);
         });
         this.dragulaService.options(this.$scope, 'lane', {
             accepts: (task, targetLane, sourceLane) => {
-                return this.canTaskBeDropped(task, targetLane, sourceLane);
+                return this.isValidTransition(sourceLane, targetLane);
             }
         });
     }
 
-    onLaneHovered(event, task, lane) {
+    onLaneHovered(lane) {
         lane.addClass('over');
     }
 
-    onLaneExited(event, task, lane) {
+    onLaneExited(lane) {
         lane.removeClass('over');
     }
 
-    onTaskDropped(event, task, targetLane, sourceLane, beforeTask) {
+    onTaskDropped(task, targetLane, sourceLane, beforeTask) {
         var taskId = task.attr('id');
         var sourceLaneId = sourceLane.attr('id');
         var targetLaneId = targetLane.attr('id');
@@ -45,11 +45,10 @@ class ProjectBoardController {
         console.log('Moved task ' + taskId + ' from lane ' + sourceLaneId + ' to lane ' + targetLaneId);
     }
 
-    canTaskBeDropped(task, targetLane, sourceLane) {
+    isValidTransition(sourceLane, targetLane) {
         var sourceLaneId = angular.element(sourceLane).attr('id');
         var targetLaneId = angular.element(targetLane).attr('id');
-        var isValidTransition = this.project.flow.transitions[sourceLaneId].indexOf(targetLaneId) >= 0;
-        return isValidTransition;
+        return this.project.flow.transitions[sourceLaneId].indexOf(targetLaneId) >= 0;
     }
 }
 
