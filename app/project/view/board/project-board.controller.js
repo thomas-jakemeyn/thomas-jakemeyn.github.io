1e394ca556f3742846a9e6c9d95184c5f12f76ae
thomas_jakemeyn <thomas.jakemeyn@gmail.com>
2015-11-01 07:52:03 +0100

    Added hover effect on lanes of board

--- app/project/view/board/project-board.controller.js
+++ app/project/view/board/project-board.controller.js
@@ -3,6 +3,12 @@
 class ProjectBoardController {
 
     constructor($scope) {
+        $scope.$on('lane.over', function (e, task, lane) {
+            lane.addClass('over');
+        });
+        $scope.$on('lane.out', function (e, task, lane) {
+            lane.removeClass('over');
+        });
     }
 }
 
