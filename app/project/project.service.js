bd2f9369d31f7ae2730ed889afa4dee40487a054
thomas_jakemeyn <thomas.jakemeyn@gmail.com>
2015-11-03 18:20:52 +0100

    Fixing bug in backlog view

--- app/project/project.service.js
+++ app/project/project.service.js
@@ -18,19 +18,19 @@ class ProjectService {
     }
 
     changeTaskPriority(project, taskId, nextTaskId, sprintId) {
-        this.backend.changeTaskPriority(project.id, taskId, nextTaskId, sprintId).then(() => {
+        return this.backend.changeTaskPriority(project.id, taskId, nextTaskId, sprintId).then(() => {
             this.projectUtils.changeTaskPriority(project, taskId, nextTaskId, sprintId);
         });
     }
 
     changeTaskState(project, taskId, stateId) {
-        this.backend.changeTaskState(project.id, taskId, stateId).then(() => {
+        return this.backend.changeTaskState(project.id, taskId, stateId).then(() => {
             this.projectUtils.changeTaskState(project, taskId, stateId);
         });
     }
 
     createTask(project, data, nextTaskId, sprintId) {
-        this.backend.createTask(project.id, data, nextTaskId, sprintId).then(task => {
+        return this.backend.createTask(project.id, data, nextTaskId, sprintId).then(task => {
             this.projectUtils.insertNewTask(project, task, nextTaskId, sprintId);
         });
     }
